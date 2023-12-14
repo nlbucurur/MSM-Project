197eb62d1a01f06c74854a861bdc5138919da1d5
Lorena Bucurú Rodríguez <nlbucurur@unal.edu.co>
2023-12-14 14:47:32 +0100

    🎨 Improve structure of the code and add some comments

--- HeavyMetals.m
+++ HeavyMetals.m
@@ -1,9 +1,13 @@
 clear all;
 
-%number of steps
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Definition of constants %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Number of steps (10 days)
 s = 5000;
 
-% transition rates (in days^-1)
+% Transition rates (in days^-1)
 a = 1/1.1; % W -> S
 b = 1/5.2; % W -> K
 c = 1/6.0; % W -> F
@@ -16,93 +20,147 @@ i = 5.0;   % S -> D
 j = 1/10.0; % K -> D
 k = 1/730; % F -> D
 
-%transition rates vector
+% Transition rates vector
 lambda = [a,b,c,d,e,f,g,h,i,j,k];
 
-% dt
+% Delta of time
 dt = (1/100)*1/(max(lambda));
 
-% transition matrix
+%%%%%%%%%%%%%%%%%%%%%%%
+%% Transition Matrix %%
+%%%%%%%%%%%%%%%%%%%%%%%
+
+%{
+    Transition matrix 5x5
+    Columns and rows order: [D]eath [W]ater [S]eaweeds [K]rill [F]ish
+%}
+
 M = [1 0 i*dt j*dt k*dt; 
     0 1-((a + b + c)*dt) d*dt e*dt f*dt;
     0 a*dt 1-((d + g + i)*dt) 0 0;
     0 b*dt g*dt 1-((e + h + j)*dt) 0;
     0 c*dt 0 h*dt 1-((f + k)*dt)];
 
-% population vector N = [D W S K F]
+%%%%%%%%%%%%%%%%%%%%
+%% Time Evolution %%
+%%%%%%%%%%%%%%%%%%%%
+
+% Population vector N = [[D]eath [W]ater [S]eaweeds [K]rill [F]ish] initialization
 N = zeros(5,s);
 
-% N(t = 0)
+% Definion of N(t = 0)
 N(:,1) = [0 1 0 0 0];
 
-% time evolution
+% Time evolution of N
 for i = 2:s
     N(:,i) = M*N(:,i-1);
 end
 
-% evolution of each state
+% Evolution of each state
 D = N(1,:);
 W = N(2,:);
 S = N(3,:);
 K = N(4,:);
 F = N(5,:);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Time evolution Plots %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% dt vector
+% Creation of dt vector (for plot purposes)
 DT = zeros(1,s);
 for i = 1:s
     DT(i) = (i-1)*dt;
 end
 
-% plot of the evolution
-plot(DT, D, 'r', DT, W, 'c', DT, S, 'g', DT, K, 'm', DT, F , 'b');
+% Plot of the evolution of the population in [D]eath [W]ater [S]eaweeds [K]rill and [F]ish
+
+plot(DT, D, 'color', "#AE431E", 'LineWidth',2);
 title("Time evolution of the State's Population");
 xlabel('t');
 ylabel('State Population');
+
+hold on
+plot(DT, W, 'color', "#8FBDD3", 'LineWidth',2);
+plot(DT, S, 'color', "#A3A847", 'LineWidth',2);
+plot(DT, K, 'color', "#A7727D", 'LineWidth',2);
+plot(DT, F, 'color', "#506D84", 'LineWidth',2);
+hold off
+
 legend({'D', 'W', 'S', 'K', 'F'}, 'Location', 'east');
 fig1 = gcf;
-exportgraphics(fig1, "population-evolution.png");
+exportgraphics(fig1, "01_population-evolution.png");
 
-% plot of the evolution loglog
-loglog(DT, D, 'r', DT, W, 'c', DT, S, 'g', DT, K, 'm', DT, F , 'b');
-title("Time evolution of the State's Population");
+% Population evolution plot in loglog
+loglog(DT, D, 'color', "#AE431E", 'LineWidth',2);
+title("Log-Log Time evolution of the State's Population");
 xlabel('t');
 ylabel('State Population');
-legend({'D', 'W', 'S', 'K', 'F'}, 'Location', 'east');
-fig5 = gcf;
-exportgraphics(fig5, "population-evolution-loglog.png");
 
-% total mass of fish, krill and seaweeds
+hold on
+loglog(DT, W, 'color', "#8FBDD3", 'LineWidth',2);
+loglog(DT, S, 'color', "#A3A847", 'LineWidth',2);
+loglog(DT, K, 'color', "#A7727D", 'LineWidth',2);
+loglog(DT, F, 'color', "#506D84", 'LineWidth',2);
+hold off
+
+legend({'D', 'W', 'S', 'K', 'F'}, 'Location', 'southeast');
+fig2 = gcf;
+exportgraphics(fig2, "02_population-evolution-loglog.png");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Relative proportions with mass %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Total mass of fish (M_F), krill (M_K) and seaweeds (M_S)
 M_F = 1;
 M_S = 24*M_F;
 M_K = 6*M_F;
 
-% relative proportions of heavy metals in living seaweeds, krill and fish
+% Relative proportions of heavy metals in living seaweeds, krill and fish
 for i = 1:length(S)
     RS(i) = M_S*S(i)/(M_S*S(i)+M_F*F(i)+M_K*K(i));
     RK(i) = M_K*K(i)/(M_S*S(i)+M_F*F(i)+M_K*K(i));
     RF(i) = M_F*F(i)/(M_S*S(i)+M_F*F(i)+M_K*K(i));
 end
 
-plot(DT, RS, 'g', DT, RK, 'm', DT, RF , 'b');
+plot(DT, RS, 'color', "#A3A847", 'LineWidth',2);
 title("Relative proportion of the Population in living beings");
 xlabel('t');
-ylabel('Relative Population in living beings');
-legend({'RS', 'RK', 'RF'}, 'Location', 'east');
-fig2 = gcf;
-exportgraphics(fig2, "relative-proportion.png");
+ylabel('Relative Population in Living Beings');
 
-% we now consider a leak of metal from a steel plant (state P) with a very small rate
+hold on
+plot(DT, RK, 'color', "#A7727D", 'LineWidth',2);
+plot(DT, RF, 'color', "#506D84", 'LineWidth',2);
+hold off
+
+legend({'RS', 'RK', 'RF'}, 'Location', 'northeast');
+fig3 = gcf;
+exportgraphics(fig3, "03_relative-proportion.png");
+
+%%%%%%%%%%%%%%%%%
+%% Steel Plant %%
+%%%%%%%%%%%%%%%%%
+
+% We now consider a leak of metal from a steel plant (state P) with a very small rate
 s2 = 9125000;
 w = 1/9125; % P -> W
 
-% new transition rates vector
+% New transition rates vector
 lambda2 = [a,b,c,d,e,f,g,h,i,j,k,w];
 
 % dt
 dt2 = (1/100)*1/(max(lambda2));
 
-% transition matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Transition Matrix 2 %%
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%{
+    Transition matrix 6x6
+    Columns and rows order: [D]eath [P]lant [W]ater [S]eaweeds [K]rill [F]ish
+%}
+
 M2 = [1 0 0 i*dt2 j*dt2 k*dt2;
     0 1-(w*dt2) 0 0 0 0;
     0 w*dt2 1-((a + b + c)*dt2) d*dt2 e*dt2 f*dt2;
@@ -110,18 +168,22 @@ M2 = [1 0 0 i*dt2 j*dt2 k*dt2;
     0 0 b*dt2 g*dt2 1-((e + h + j)*dt2) 0;
     0 0 c*dt2 0 h*dt2 1-((f + k)*dt2)];
 
-% population vector N = [D W S K F]
+% Population vector N = [[D]ead [W]ater [S]eaweeds [K]rill [F]ish] initialization
 N2 = zeros(6,s2);
 
 % N(t = 0)
 N2(:,1) = [0 1 0 0 0 0];
 
-% time evolution
+%%%%%%%%%%%%%%%%%%%%
+%% Time Evolution %%
+%%%%%%%%%%%%%%%%%%%%
+
+% Time evolution
 for i = 2:s2
     N2(:,i) = M2*N2(:,i-1);
 end
 
-% evolution of each state
+% Evolution of each state
 D2 = N2(1,:);
 P2 = N2(2,:);
 W2 = N2(3,:);
@@ -135,24 +197,50 @@ for i = 1:s2
     DT2(i) = (i-1)*dt2;
 end
 
-% plot of the evolution
-plot(DT2, D2, 'r', DT2, P2, 'k', DT2, W2, 'c', DT2, S2, 'g', DT2, K2, 'm', DT2, F2 , 'b');
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Time Evolution Plots %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Plot of the evolution
+plot(DT2, D2, 'color', "#AE431E", 'LineWidth',2);
 title("Time evolution of the State's Population considering leak");
 xlabel('t');
 ylabel('State Population');
-legend({'D', 'P', 'W', 'S', 'K', 'F'}, 'Location', 'east');
-fig3 = gcf;
-exportgraphics(fig3, "population-evolution-leak.png");
 
-% plot of the evolution (log-log)
-loglog(DT2, D2, 'r', DT2, P2, 'k', DT2, W2, 'c', DT2, S2, 'g', DT2, K2, 'm', DT2, F2 , 'b');
+hold on
+plot(DT2, P2, 'color', "#65451F", 'LineWidth',2);
+plot(DT2, W2, 'color', "#8FBDD3", 'LineWidth',2);
+plot(DT2, S2, 'color', "#A3A847", 'LineWidth',2);
+plot(DT2, K2, 'color', "#A7727D", 'LineWidth',2);
+plot(DT2, F2, 'color', "#506D84", 'LineWidth',2);
+hold off
+
+legend({'D', 'P', 'W', 'S', 'K', 'F'}, 'Location', 'east');
+fig4 = gcf;
+exportgraphics(fig4, "04_population-evolution-leak.png");
+
+% Plot of the evolution (log-log)
+loglog(DT2, D2, 'color', "#AE431E", 'LineWidth',2);
 title("Time evolution of the State's Population considering leak log-log");
 xlabel('t');
 ylabel('State Population');
-legend({'D', 'P', 'W', 'S', 'K', 'F'}, 'Location', 'east');
-fig4 = gcf;
-exportgraphics(fig4, "population-evolution-leak-loglog.png");
 
-% calculation of M^n
+hold on
+loglog(DT2, P2, 'color', "#65451F", 'LineWidth',2);
+loglog(DT2, W2, 'color', "#8FBDD3", 'LineWidth',2);
+loglog(DT2, S2, 'color', "#A3A847", 'LineWidth',2);
+loglog(DT2, K2, 'color', "#A7727D", 'LineWidth',2);
+loglog(DT2, F2, 'color', "#506D84", 'LineWidth',2);
+hold off
+
+legend({'D', 'P', 'W', 'S', 'K', 'F'}, 'Location', 'east');
+fig5 = gcf;
+exportgraphics(fig5, "05_population-evolution-leak-loglog.png");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Transition Matrix and Absorbtion Matrix %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Calculation of M^n
 Q = M(2:5, 2:5);
 R = M(1, 2:5);
